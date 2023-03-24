09a97708a310371da247b471b0f804ce6888b47e
george <georgejordan1973@icloud.com>
2023-03-23 19:04:54 -0500

    deleted my changes

--- public/js/api.js
+++ public/js/api.js
@@ -1,22 +1,22 @@
-const axios = require("axios");
+// const axios = require("axios");
 
-const options = {
-  method: 'GET',
-  url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients',
-  params: {
-    ingredients: 'apples,flour,sugar',
-    number: '5',
-    ignorePantry: 'true',
-    ranking: '1'
-  },
-  headers: {
-    'X-RapidAPI-Key': '1d47bbecfemsh8c3f3c33a8cbb3ap10a22ajsn34759b2ed613',
-    'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
-  }
-};
+// const options = {
+//   method: 'GET',
+//   url: 'https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients',
+//   params: {
+//     ingredients: 'apples,flour,sugar',
+//     number: '5',
+//     ignorePantry: 'true',
+//     ranking: '1'
+//   },
+//   headers: {
+//     'X-RapidAPI-Key': '1d47bbecfemsh8c3f3c33a8cbb3ap10a22ajsn34759b2ed613',
+//     'X-RapidAPI-Host': 'spoonacular-recipe-food-nutrition-v1.p.rapidapi.com'
+//   }
+// };
 
-axios.request(options).then(function (response) {
-	console.log(response.data);
-}).catch(function (error) {
-	console.error(error);
-});
+// axios.request(options).then(function (response) {
+// 	console.log(response.data);
+// }).catch(function (error) {
+// 	console.error(error);
+// });
