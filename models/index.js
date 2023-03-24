91ed9c0d4ee7fabbf23b89eb072873a923ac7ac4
Cj adorna <cjadorna@hotmail.com>
2023-03-23 21:05:22 -0500

    recipe models created to save to user

--- models/index.js
+++ models/index.js
@@ -1,6 +1,14 @@
 const User = require('./User');
+const SavedUserRecipes = require('./recipe');
+
+User.hasMany(SavedUserRecipes, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+  });
+  
+  SavedUserRecipes.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
 
 
-
-
-module.exports = { User };
+module.exports = { User, SavedUserRecipes };
